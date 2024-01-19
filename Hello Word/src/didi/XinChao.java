358775a0b06e5d7f4d9313ea0e2325971ae7f8de
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-01-19 14:08:12 +0700

    Demo
    Demo

---
+++ Hello Word/src/didi/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package didi;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Toi lan dau hoc Java, va dang thu nghiem quan ly ma nguon voi GIT");
+	}
+
+}
