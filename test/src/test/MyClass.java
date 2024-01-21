3dace309ba3c5299d903b109fb561b8d0a1e0cd9
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-01-21 21:55:57 +0700

    demo

--- test/src/test/MyClass.java
+++ test/src/test/MyClass.java
@@ -1,10 +1,7 @@
 package test;
 
 public class MyClass {
- public static void main(String[] args) {
-	 int a = 15;
-	 int b = 5;
-	 int c = a + b;
-	 System.out.println("Tong: "+c);
- }
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+	}
 }
